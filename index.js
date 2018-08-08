127fbc5d4beb0852cb16d08a31ff9e547524b86e
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-08-08 05:54:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,5 @@ function BoardMember (name, homeState, training){
   this.releasePressStatement= function(){ return 'You will see great things from Scuber.';} ;
   this.sayHi= function(){ return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;};
 }
+let polishedBoardMember = new BoardMember('Polished', 'New York', 'law.');
+let polishedBoardMember = new BoardMember('Polished', 'New York', 'law.');
