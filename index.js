60e63684829d5741616b82ade393592bc733c1a7
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-08-08 05:56:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function BoardMember (name, homeState, training){
   this.name=name;
   this.homeState=homeState;
   this.training=training;
-  this.veto= function(){ return 'No, I must disagree';};
+  this.veto= function(){ return 'No, I must disagree'}
   this.approve= function(){ return 'You can do that!';};
   this.doCharity = function(){ return 'I like to help people.' ;};
   this.releasePressStatement= function(){ return 'You will see great things from Scuber.';} ;
