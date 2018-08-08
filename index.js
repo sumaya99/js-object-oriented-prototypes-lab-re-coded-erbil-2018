cd623ae44631ffc903b01a9c62c47cfd1a999cb7
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-08-08 06:00:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ function BoardMember (name, homeState, training){
   this.homeState=homeState;
   this.training=training;
   BoardMember.prototype.veto= function(){ return 'No, I must disagree'}
-  this.approve= function(){ return 'You can do that!'}
-  this.doCharity = function(){ return 'I like to help people.' }
-  this.releasePressStatement= function(){ return 'You will see great things from Scuber.'}
-  this.sayHi= function(){ return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`}
+  BoardMember.prototype.approve= function(){ return 'You can do that!'}
+  BoardMember.prototype.doCharity = function(){ return 'I like to help people.' }
+  BoardMember.prototype.releasePressStatement= function(){ return 'You will see great things from Scuber.'}
+  BoardMember.prototype.sayHi= function(){ return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`}
 }
 let polishedBoardMember = new BoardMember('Polished', 'New York', 'law.');
 let backSlappingGene  = new BoardMember('Polished', 'New York', 'law.');
