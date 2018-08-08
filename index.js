6f219cb20f9076b2b1fd871d0d18b7ecd591fe38
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-08-08 05:55:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ function BoardMember (name, homeState, training){
   this.sayHi= function(){ return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;};
 }
 let polishedBoardMember = new BoardMember('Polished', 'New York', 'law.');
-let polishedBoardMember = new BoardMember('Polished', 'New York', 'law.');
+let backSlappingGene  = new BoardMember('Polished', 'New York', 'law.');
